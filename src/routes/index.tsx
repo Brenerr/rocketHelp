414eeaef3ba0c5cd1e9530d45f07a23da033ed66
Brener Alves <brener820@gmail.com>
2022-07-28 16:53:03 -0300

    Aula 4 - Integração com Firebase e finalização

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,32 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import { AppRoutes } from './app.routes';
 
+import { Loading } from '../components/Loading';
+import { SignIn } from '../screens/SignIn';
+
 export function Routes() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+    useEffect(() => {
+        const subcriber = auth()
+            .onAuthStateChanged(response => {
+                setUser(response);
+                setIsLoading(false);
+            });
+
+        return subcriber;
+    }, []);
+
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
         <NavigationContainer>
-            <AppRoutes />
+            {user ? <AppRoutes /> : <SignIn />}
         </NavigationContainer>
     );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth'
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -8,10 +10,37 @@ import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
 export function SignIn() {
-    const [name, setName] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const { colors } = useTheme();
+
+    function handleSignIn() {
+        if (!email || !password) {
+            return Alert.alert('Entrar', 'Informe e-mail e senha.');
+        }
+
+        setIsLoading(true);
+
+        auth().signInWithEmailAndPassword(email, password)
+            .catch((error) => {
+                console.log(error);
+                setIsLoading(false);
+
+                if (error.code === 'auth/invalid-email') {
+                    return Alert.alert('Entrar', 'E-mail inválido.');
+                }
+
+                if (error.code === 'auth/wrong-password') {
+                    return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+                }
+
+                if (error.code === 'auth/user-not-found') {
+                    return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+                }
+            })
+    }
 
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -25,7 +54,7 @@ export function SignIn() {
                 mb={4}
                 placeholder="E-mail"
                 InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-                onChangeText={setName}
+                onChangeText={setEmail}
             />
 
             <Input
@@ -36,7 +65,12 @@ export function SignIn() {
                 onChangeText={setPassword}
             />
 
-            <Button title="Entrar" w="full" />
+            <Button
+                title="Entrar"
+                w="full"
+                onPress={handleSignIn}
+                isLoading={isLoading}
+            />
         </VStack>
     );
 }
